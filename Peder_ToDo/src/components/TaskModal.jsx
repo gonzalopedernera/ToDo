1156801a3d68a3cf78f13df23cd1714a7a11d89d
gonzalopedernera <gonzalopeder2@gmail.com>
2024-03-15 22:11:45 +0000

    Modified the component to change when task is completed

--- Peder_ToDo/src/components/TaskModal.jsx
+++ Peder_ToDo/src/components/TaskModal.jsx
@@ -1,17 +1,22 @@
 import { useModal } from "../context/modal"
 
-const TaskModal = ({ text, img, children, isBttn, id }) => {
+const TaskModal = ({ text, img, children, isBttn, id, isCompleted}) => {
   const { isModalOpen, openModal } = useModal()
 
   return (
     <div>
-      <button className={`${isBttn ? 'btn btn-outline btn-primary' : 'p-5 text-black bg-purple-400 min-w-72'}`} onClick={() => openModal(id)}>{
+      <button className={`${isBttn ? 
+      'btn  btn-neutral' : 
+      `p-5 text-black min-w-72 justify-center items-start ${isCompleted ? 'line-through bgTaskCompleted' : 'bgTask'}`}`} 
+      onClick={() => openModal(id)}>
+        {
         img ? <img src={img} alt={text} className='svg-icon'/> : text
-      }</button>
+      }
+      </button>
       {isModalOpen(id) ?
       (
-      <div className='flex fixed bg-black justify-center items-center w-full h-full top-0 left-0 bg-opacity-60 backdrop-blur-sm'>
-        <div className='flex bg-white opacity-100 text-black'>
+      <div className='flex fixed bg-black  justify-center items-center w-full h-full top-0 left-0 bg-opacity-60 backdrop-blur-sm'>
+        <div className='flex bg-white border border-black text-black rounded-2xl justify-center items-center'>
           {children}
         </div>
       </div>
