8e82475a845d6be8b4aea045608f2841902e666f
gonzalopedernera <gonzalopeder2@gmail.com>
2024-03-15 22:05:00 +0000

    Added alert context provider on main file

--- Peder_ToDo/src/main.jsx
+++ Peder_ToDo/src/main.jsx
@@ -3,9 +3,12 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { ModalProvider } from './context/modal.jsx'
+import { AlertProvider } from './context/alert.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <ModalProvider>
-    <App />
+    <AlertProvider>
+      <App />
+    </AlertProvider>
   </ModalProvider>
 )
